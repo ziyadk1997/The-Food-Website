a510ab6e78b7451e48c8668ad6c97e61b29d75d6
mash197 <mohamedashraf8997@gmail.com>
2017-07-20 21:24:58 +0200

    finalized adding broadcast

--- The Food Website Backend/FoodWebsite/Scripts/myscript.js
+++ The Food Website Backend/FoodWebsite/Scripts/myscript.js
@@ -1,39 +1,42 @@
 
+function LoadRestaurants(restList) {
+    $('#orderSetupItems li:not(:first)').remove();
+    for (var i = 0; i < restList.length; i++) {
+        var name = restList[i].Name;
+        $("#orderSetupItems").append("<li class=\"restButton list-group-item\" restId = " + restList[i].RestaurantID + ">" + name + "</li>");
 
-	
-	
-	function initializeOrderList() 
-	{
+    }
+}
+
+
+function initializeOrderList() {
     var input, filter, ul, li, a, i;
     input = document.getElementById("orderSetupFilterList");
     filter = input.value.toUpperCase();
     ul = document.getElementById("orderSetupItems");
-	ul.style.display="";
+    ul.style.display = "";
     li = ul.getElementsByTagName('li');
 
-    for (i = 0; i < li.length; i++) 
-	{
-        a = li[i].getElementsByTagName("a")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+    for (i = 0; i < li.length; i++) {
+        a = li[i];
+        if (a == null || a.innerHTML.toUpperCase().indexOf(filter) > -1) {
             li[i].style.display = "";
         } else {
             li[i].style.display = "none";
         }
     }
-	
-	}
-	
-	function initializeItemList() 
-	{
+
+}
+
+function initializeItemList() {
     var input, filter, ul, li, a, i;
     input = document.getElementById("ItemSetupFilterList");
     filter = input.value.toUpperCase();
     ul = document.getElementById("ItemSetupItems");
-	ul.style.display="";
+    ul.style.display = "";
     li = ul.getElementsByTagName('li');
 
-    for (i = 0; i < li.length; i++) 
-	{
+    for (i = 0; i < li.length; i++) {
         a = li[i].getElementsByTagName("a")[0];
         if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
             li[i].style.display = "";
@@ -41,169 +44,154 @@
             li[i].style.display = "none";
         }
     }
-	
-	}
 
- $(document).ready(function(){
-	 
-	 
-  $("#history").hide();
-  $("#history_select").hide();
-  $("#home_select").hide();
-  $("home").show();
-  $("#add_btn").show();
-  $("#check_btn").hide();
-  $("#recipt").hide();
-  $("#order_setup").hide();
-  
-  
-  
-  
-  $("#ItemSetupItems li a ").click(function()
-    {
-	  $("#ItemSetupItems").hide();
-	  $("#ItemSetupFilterList").val(this.innerText);
+}
+
+$(document).ready(function () {
+
+
+    $("#history").hide();
+    $("#history_select").hide();
+    $("#home_select").hide();
+    $("home").show();
+    $("#add_btn").show();
+    $("#check_btn").hide();
+    $("#recipt").hide();
+    $("#order_setup").hide();
+
+
+
+
+    $("#ItemSetupItems li a ").click(function () {
+        $("#ItemSetupItems").hide();
+        $("#ItemSetupFilterList").val(this.innerText);
     });
-	
-	 $("#orderSetupItems li a ").click(function()
-    {
-	  $("#orderSetupItems").hide();
-	  $("#orderSetupFilterList").val(this.innerText);
+
+    $("#orderSetupItems").on("click", ".restButton", function () {
+        $("#orderSetupItems").hide();
+        $("#orderSetupFilterList").val(this.innerText);
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
     });
-	
-	
-	
-	
-	
-	$("#history_btn").click(function()
-	{	
-			$("#history").show();
-			$("#history_select").hide();
-			$("#home_select").hide();
-			$("#home").hide();	
-			$("#add_btn").hide();
-			$("#check_btn").hide();
-			$("#recipt").hide();
-			$("#order_setup").hide();	
-	});
-	$("#close_order").click(function()
-	{	
-			$("#history").show();
-			$("#history_select").hide();
-			$("#home_select").hide();
-			$("#home").hide();	
-			$("#add_btn").hide();
-			$("#check_btn").hide();
-			$("#recipt").hide();
-			$("#order_setup").hide();	
-	});
-	
-	$("#details").click(function()
-	{
-			var details=prompt("please type any special comments you would like to include in the order");
-	});
-	$(".order_details").click(function()
-	{
-			$("#history").hide();
-			$("#history_select").hide();
-			$("#home_select").hide();
-			$("#home").hide();	
-			$("#add_btn").hide();
-			$("#check_btn").hide();
-			$("#recipt").show();
-			$("#order_setup").hide();
-	});
-	$("#current_order_btn").click(function()
-	{
-			$("#history").hide();
-			$("#history_select").hide();
-			$("#home_select").hide();
-			$("#home").show();
-			$("#add_btn").show();
-			$("#check_btn").hide();
-			$("#recipt").hide();
-			$("#order_setup").hide();			
-	});
-	$("#add_btn").on("click", function()
-	{
-			$("#history").hide();
-			$("#history_select").hide();
-			$("#home_select").hide();
-			$("#home").hide();
-			$("#add_btn").hide();
-			$("#check_btn").hide();
-			$("#recipt").hide();
-			$("#order_setup").show();
-	});
-	$("#done").on("click", function()
-	{
-			$("#history").hide();
-			$("#history_select").hide();
-			$("#home_select").hide();
-			$("#home").show();
-			$("#add_btn").show();
-			$("#check_btn").hide();
-			$("#recipt").hide();
-			$("#order_setup").hide();
-			var name=prompt("Please enter your name");
-			var btn=document.createElement("BUTTON");
-			btn.innerHTML=name+"<br>";
-			btn.innerHTML += $("#timepicker1").val();
-			btn.innerHTML += "<br>";
-			btn.innerHTML += $("#orderSetupFilterList").val();
-			btn.setAttribute("class","btn btn-primary header orders");
-			var table=$("#home_table");
-			table.append(btn);
-	});
-	$(".orders").click(function()
-	{
-			
-			$("#history").hide();
-			$("#history_select").hide();
-			$("#home_select").show();
-			$("#home").hide();
-			$("#add_btn").hide();
-			$("#check_btn").show();
-			$("#recipt").hide();
-			$("#order_setup").hide();
-	});
-	$("#add_restaurant").click(function()
-	{
-			var new_res=prompt("Please enter the restaurant name");
-			this.innerText=new_res;
-	});
-	$("#add_item").click(function()
-	{
-			var new_item=prompt("Please enter the item name");
-			this.innerText=new_item;
-	});
-	$(".pay").click(function()
-	{
-			
-			$("#history").hide();
-			$("#history_select").show();
-			$("#home_select").hide();
-			$("#home").hide();
-			$("#add_btn").hide();
-			$("#check_btn").hide();
-			$("#recipt").hide();
-	});
-	$("#check_btn").click(function()
-	{
-			$("#history").hide();
-			$("#history_select").hide();
-			$("#home_select").hide();
-			$("#home").show();
-			$("#add_btn").show();
-			$("#check_btn").hide();
-			$("#recipt").hide();
-			$("#order_setup").hide();
-	});
-  });
-  
- 
-  
-  
-  
-  
-  
+
+
+
+
+
+    $("#history_btn").click(function () {
+        $("#history").show();
+        $("#history_select").hide();
+        $("#home_select").hide();
+        $("#home").hide();
+        $("#add_btn").hide();
+        $("#check_btn").hide();
+        $("#recipt").hide();
+        $("#order_setup").hide();
+    });
+    $("#close_order").click(function () {
+        $("#history").show();
+        $("#history_select").hide();
+        $("#home_select").hide();
+        $("#home").hide();
+        $("#add_btn").hide();
+        $("#check_btn").hide();
+        $("#recipt").hide();
+        $("#order_setup").hide();
+    });
+
+    $("#details").click(function () {
+        var details = prompt("please type any special comments you would like to include in the order");
+    });
+    $(".order_details").click(function () {
+        $("#history").hide();
+        $("#history_select").hide();
+        $("#home_select").hide();
+        $("#home").hide();
+        $("#add_btn").hide();
+        $("#check_btn").hide();
+        $("#recipt").show();
+        $("#order_setup").hide();
+    });
+    $("#current_order_btn").click(function () {
+        $("#history").hide();
+        $("#history_select").hide();
+        $("#home_select").hide();
+        $("#home").show();
+        $("#add_btn").show();
+        $("#check_btn").hide();
+        $("#recipt").hide();
+        $("#order_setup").hide();
+    });
+    $("#add_btn").on("click", function () {
+        $("#history").hide();
+        $("#history_select").hide();
+        $("#home_select").hide();
+        $("#home").hide();
+        $("#add_btn").hide();
+        $("#check_btn").hide();
+        $("#recipt").hide();
+        $("#order_setup").show();
+        GetRestaurants();
+    });
+    $("#done").on("click", function () {
+        $("#history").hide();
+        $("#history_select").hide();
+        $("#home_select").hide();
+        $("#home").show();
+        $("#add_btn").show();
+        $("#check_btn").hide();
+        $("#recipt").hide();
+        $("#order_setup").hide();
+        var name = prompt("Please enter your name");
+        var id = $($("#orderSetupItems").find("li.active")).attr("restid");
+        var deadline = $("#timepicker1").val();
+        AddBroadcast(id, deadline);
+    });
+    $(".orders").click(function () {
+
+        $("#history").hide();
+        $("#history_select").hide();
+        $("#home_select").show();
+        $("#home").hide();
+        $("#add_btn").hide();
+        $("#check_btn").show();
+        $("#recipt").hide();
+        $("#order_setup").hide();
+    });
+    $("#add_restaurant").click(function () {
+        var new_res = prompt("Please enter the restaurant name");
+        $('#orderSetupFilterList').val(new_res);
+        AddRestaurant(new_res);
+    });
+    $("#add_item").click(function () {
+        var new_item = prompt("Please enter the item name");
+        this.innerText = new_item;
+
+    });
+    $(".pay").click(function () {
+
+        $("#history").hide();
+        $("#history_select").show();
+        $("#home_select").hide();
+        $("#home").hide();
+        $("#add_btn").hide();
+        $("#check_btn").hide();
+        $("#recipt").hide();
+    });
+    $("#check_btn").click(function () {
+        $("#history").hide();
+        $("#history_select").hide();
+        $("#home_select").hide();
+        $("#home").show();
+        $("#add_btn").show();
+        $("#check_btn").hide();
+        $("#recipt").hide();
+        $("#order_setup").hide();
+    });
+});
+
+
+
+
+
+
