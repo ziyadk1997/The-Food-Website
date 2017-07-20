2e883aa72a92838b62cba2a01e0e0e4762e13b2a
ziyad khaled <ziyadk@windowslive.com>
2017-07-20 16:48:48 +0200

    Moved FE inside asp net razor views

---
+++ The Food Website Backend/FoodWebsite/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodWebsite.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public ActionResult Home()
+        {
+            return View();
+        }
+    }
+}
